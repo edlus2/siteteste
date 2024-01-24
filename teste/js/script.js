24394c0fac6243823abff3593861fae199797047
edlus2 <edgar.ludis2@gmail.com>
2024-01-24 00:37:10 -0300

    att

--- teste/js/script.js
+++ teste/js/script.js
@@ -1,7 +1,16 @@
-let inte = document.querySelector(".interrupto");
+let inte = document.getElementById("interrupto");
+let interruptooff = document.getElementById("interruptooff");
+let interruptoon = document.getElementById("interruptoon");
+inte.addEventListener('click',apert);
 
-inte.addEventListener('lick',onclick);
 
-function lick(){
- alert('ola')
+
+
+function apert(){
+    if(interruptooff === display.none){
+        interruptooff.style.display= 'block';
+    }else
+
+    interruptooff.style.display ='none';
+    interruptoon.style.display = 'block';
 }
