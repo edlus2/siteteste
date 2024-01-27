d33788b8d7bc0cad90391ae6b7b6599e5fdd5773
edlus2 <edgar.ludis2@gmail.com>
2024-01-27 10:42:21 -0300

    att
    criando estante de figure action.

--- teste/js/script.js
+++ teste/js/script.js
@@ -1,12 +1,25 @@
 let off = window.document.querySelector('.interruptooff')
 let on = window.document.querySelector('.interruptoon')
 
+let led1 = window.document.querySelector('.est')
+let led2 = window.document.querySelector('.esta')
+let led3 = window.document.querySelector('.estan')
+
+
+
 off.addEventListener('click',function(){
     off.style.display = 'none';
     on.style.display = 'block';
+    led1.className = 'est1';  
+    led2.className = 'est2';
+    led3.className = 'est3';
+
 });
 
 on.addEventListener('click',function(){
     off.style.display = 'block';
     on.style.display = 'none';
+    led1.className = 'estas';
+    led2.className = 'estas';
+    led3.className = 'estas';
 });
