c8cf5c3a7468a55458f1c77ae4669aad35b94076
edlus2 <edgar.ludis2@gmail.com>
2024-01-28 18:12:14 -0300

    att

--- teste/js/script.js
+++ teste/js/script.js
@@ -1,6 +1,8 @@
+// botão on e off
 let off = window.document.querySelector('.interruptooff')
 let on = window.document.querySelector('.interruptoon')
 
+//led 3 divisorias
 let led1 = window.document.querySelector('.est')
 let led2 = window.document.querySelector('.esta')
 let led3 = window.document.querySelector('.estan')
@@ -8,8 +10,11 @@ let led3 = window.document.querySelector('.estan')
 
 
 off.addEventListener('click',function(){
+    // botão on e off
     off.style.display = 'none';
     on.style.display = 'block';
+    
+    //led 3 divisorias
     led1.className = 'est1';  
     led2.className = 'est2';
     led3.className = 'est3';
@@ -17,8 +22,11 @@ off.addEventListener('click',function(){
 });
 
 on.addEventListener('click',function(){
+    // botão on e off
     off.style.display = 'block';
     on.style.display = 'none';
+
+    //led 3 divisorias
     led1.className = 'estas';
     led2.className = 'estas';
     led3.className = 'estas';
